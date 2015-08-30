c4f632b79a509f537854472f1a9b5f7a8955ab1f
Martin Ueding <dev@martin-ueding.de>
2015-08-30 12:50:35 +0200

    Implement crawler and ORM

--- crawler.py
+++ crawler.py
@@ -4,11 +4,131 @@
 # Copyright © 2015 Martin Ueding <dev@martin-ueding.de>
 
 import argparse
+import itertools
+import os
+import re
+import sys
 
 import requests
+import lxml.etree
+from sqlalchemy.orm import sessionmaker
+
+from model import *
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+base = 'http://stundenplan.hhg-bonn.de/klassen/'
+
+url_forms = 'http://stundenplan.hhg-bonn.de/klassen/stundenplanklassen.htm'
+url_teachers = 'http://stundenplan.hhg-bonn.de/lehrer/stundenplanlehrer.htm'
+
+
+def get_url(url, user, password):
+    cache = os.path.basename(url)
+    if not os.path.isfile(cache):
+        r = requests.get(url, auth=(user, password))
+        with open(cache, 'w') as f:
+            f.write(r.text)
+
+    with open(cache) as f:
+        text = f.read()
+
+    return text
+
+def parse_form(form_str, text):
+    form = session.query(Form).filter(Form.name == form_str).scalar()
+    if form is None:
+        form = Form(name=form_str)
+        session.add(form)
+
+    parser = lxml.etree.HTMLParser()
+    root = lxml.etree.fromstring(text, parser)
+
+    table = root.findall('.//table')[1]
+    rows = table.findall('tr')
+
+    hour = 0
+
+    #print(rows)
+
+    for row in rows:
+        cells = row.findall('td')
+        #print(cells)
+
+        if len(cells) == 0:
+            print('Skip empty row')
+            continue
+
+        for day, cell in zip(itertools.count(), cells):
+            subtable = cell.find('table')
+            #print(subtable)
+
+            sub_rows = subtable.findall('tr')
+            #print(sub_rows)
+            for sub_row in sub_rows:
+                sub_words = [x.text for x in sub_row.findall('.//b')]
+
+                if day == 0:
+                    if len(sub_words) == 1:
+                        hour = sub_words[0]
+                else:
+                    print(day, hour, sub_words)
+
+                    if len(sub_words) == 3:
+                        subject_str, teacher_str, room_str = sub_words
+
+                        subject = session.query(Subject).filter(Subject.name == subject_str).scalar()
+                        if subject is None:
+                            subject = Subject(name=subject_str)
+                            session.add(subject)
+
+                        teacher = session.query(Teacher).filter(Teacher.name == teacher_str).scalar()
+                        if teacher is None:
+                            teacher = Teacher(name=teacher_str)
+                            session.add(teacher)
+
+                        room = session.query(Room).filter(Room.name == room_str).scalar()
+                        if room is None:
+                            room = Room(name=room_str)
+                            session.add(room)
+
+                        lesson = Lesson(hour=hour, weekday=day, room=room, subject=subject, teacher=teacher, form=form)
+
+
+                        session.commit()
+
+
+    #print(lxml.etree.tostring(table, pretty_print=True, method='html').decode())
+
+    sys.exit(0)
 
 def main():
     options = _parse_args()
+
+    text = get_url(url_forms, options.user, options.password)
+
+    parser = lxml.etree.HTMLParser()
+    root = lxml.etree.fromstring(text, parser)
+
+    form_pattern = re.compile(r'stundenplanklassen_0?(\d+\w?).htm')
+
+    table = root.findall('.//table')[1]
+    urls = table.findall('.//a')
+
+    for url in urls:
+        href = url.get('href')
+
+        full_url = base + href
+        print(full_url)
+
+        m = form_pattern.match(href)
+
+        text = get_url(full_url, options.user, options.password)
+        parse_form(m.group(1), text)
+
+
+
 
 def _parse_args():
     '''
@@ -18,6 +138,8 @@ def _parse_args():
     :rtype: Namespace
     '''
     parser = argparse.ArgumentParser(description='')
+    parser.add_argument('user')
+    parser.add_argument('password')
     options = parser.parse_args()
 
     return options
--- model.py
+++ model.py
@@ -6,7 +6,8 @@
 import sqlalchemy
 import sqlalchemy.ext.declarative
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 engine = sqlalchemy.create_engine('sqlite:///hhg.sqlite', echo=True)
 
@@ -16,24 +17,43 @@ class Room(Base):
     __tablename__ = 'rooms'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, unique=True)
 
 class Teacher(Base):
     __tablename__ = 'teachers'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, unique=True)
 
 class Subject(Base):
     __tablename__ = 'subjects'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, unique=True)
+
+class Form(Base):
+    __tablename__ = 'forms'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
 
 class Lesson(Base):
     __tablename__ = 'lessons'
 
     id = Column(Integer, primary_key=True)
+    hour = Column(Integer)
+    weekday = Column(Integer)
+
+    form_id = Column(Integer, ForeignKey('forms.id'))
+    form = relationship("Form", backref=backref('lessons', order_by=id))
+
+    room_id = Column(Integer, ForeignKey('rooms.id'))
     room = relationship("Room", backref=backref('lessons', order_by=id))
+
+    subject_id = Column(Integer, ForeignKey('subjects.id'))
     subject = relationship("Subject", backref=backref('lessons', order_by=id))
+
+    teacher_id = Column(Integer, ForeignKey('teachers.id'))
     teacher = relationship("Teacher", backref=backref('lessons', order_by=id))
+
+Base.metadata.create_all(engine)
